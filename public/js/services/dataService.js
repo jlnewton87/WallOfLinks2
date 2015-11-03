d1d3195f01bfcb0c2e31ba6c03d314bd72c41f76
Josh Newton <josh.newton@careerbuilder.com>
2015-11-03 08:44:43 -0500

    removing fake data from dataService

--- public/js/services/dataService.js
+++ public/js/services/dataService.js
@@ -27,44 +27,4 @@ function getPanels($http){
 				console.log('could not get panels?! \n' + err)
 			}
 		);
-		
-		/*return [
-				{
-					title: "Panel 1",
-					_id: 'e81eb282-0981-49d6-bf2e-8263ef999b83',
-					order: 1,
-					links: [
-						{href: "https://google.com", text: "Google1", order: 1},
-						{href: "https://google.com", text: "Google2", order: 2}
-					]
-				},
-				{
-					title: "Panel 2",
-					_id: '009628a4-57d0-4955-913c-914e56f3bbdf',
-					order: 2,
-					links: [
-						{href: "https://google.com", text: "Google1", order: 1},
-						{href: "https://google.com", text: "Google2", order: 2}
-					]
-				},
-				{
-					title: "Panel 3",
-					_id: '009628a4-57d0-4955-913c-914e56f3bbdf',
-					order: 2,
-					links: [
-						{href: "https://google.com", text: "Google1", order: 1},
-						{href: "https://google.com", text: "Google2", order: 2}
-					]
-				},
-				{
-					title: "Panel 4",
-					_id: '009628a4-57d0-4955-913c-914e56f3bbdf',
-					order: 2,
-					links: [
-						{href: "https://google.com", text: "Google1", order: 1},
-						{href: "https://google.com", text: "Google2", order: 2}
-					]
-				}
-			];
-		};*/
 };
